62439af41a8581544456eec871460824d6f56166
Fatih Dumanli <fatihdumanli@live.com>
2023-03-21 15:28:54 +0300

    Process PR comments and fixes

--- src/Lykke.Snow.Notifications.Domain/Repositories/IDeviceRegistrationRepository.cs
+++ src/Lykke.Snow.Notifications.Domain/Repositories/IDeviceRegistrationRepository.cs
@@ -1,16 +1,14 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Lykke.Snow.Common.Model;
-using Lykke.Snow.Notifications.Domain.Enums;
 using Lykke.Snow.Notifications.Domain.Model;
 
 namespace Lykke.Snow.Notifications.Domain.Repositories
 {
     public interface IDeviceRegistrationRepository
     {
-        Task<Result<DeviceRegistration, DeviceRegistrationErrorCode>> GetDeviceRegistrationAsync(string deviceToken);
-        Task<Result<IEnumerable<DeviceRegistration>, DeviceRegistrationErrorCode>> GetDeviceRegistrationsByAccountIdAsync(string accountId);
-        Task<Result<DeviceRegistrationErrorCode>> InsertAsync(DeviceRegistration entity);
-        Task<Result<DeviceRegistrationErrorCode>> DeleteAsync(string deviceToken);
+        Task<DeviceRegistration> GetDeviceRegistrationAsync(string deviceToken);
+        Task<IReadOnlyList<DeviceRegistration>> GetDeviceRegistrationsByAccountIdAsync(string accountId);
+        Task InsertAsync(DeviceRegistration entity);
+        Task DeleteAsync(int oid);
     }
 }
--- tests/Lykke.Snow.Notifications.Tests/Repository/DeviceRegistrationRepositoryTests.cs
+++ tests/Lykke.Snow.Notifications.Tests/Repository/DeviceRegistrationRepositoryTests.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Threading.Tasks;
 using AutoMapper;
 using Lykke.Snow.Notifications.Domain.Enums;
 using Lykke.Snow.Notifications.Domain.Model;
 using Lykke.Snow.Notifications.SqlRepositories.Entities;
+using Lykke.Snow.Notifications.SqlRepositories.Exceptions;
 using Lykke.Snow.Notifications.SqlRepositories.Repositories;
 using Lykke.Snow.Notifications.Tests.Fakes;
 using Moq;
@@ -13,21 +15,17 @@ namespace Lykke.Snow.Notifications.Tests.Repository
     public class DeviceRegistrationRepositoryTests
     {
         [Fact]
-        public async Task GetDeviceRegistrationAsync_ShouldReturnDoesntExist_WhenResultIsNull()
+        public async Task GetDeviceRegistrationAsync_ShouldThrow_EntityNotFoundException_WhenEntityIsNull()
         {
             var sut = CreateSut();
             
-            var result = await sut.GetDeviceRegistrationAsync("device-token-that-does-not-exist-on-database");
-            
-            Assert.True(result.IsFailed);
-            Assert.False(result.IsSuccess);
-            Assert.Equal(DeviceRegistrationErrorCode.DoesNotExist, result.Error);
+            await Assert.ThrowsAsync<EntityNotFoundException>(async() => await sut.GetDeviceRegistrationAsync("device-token-that-does-not-exist-on-database"));
         }
         
         private DeviceRegistrationRepository CreateSut()
         {
             var mockMapper = new Mock<IMapper>();
-            mockMapper.Setup(x => x.Map<DeviceRegistration>(It.IsAny<DeviceRegistrationEntity>())).Returns(new DeviceRegistration("account-id", "device-token"));
+            mockMapper.Setup(x => x.Map<DeviceRegistration>(It.IsAny<DeviceRegistrationEntity>())).Returns(new DeviceRegistration("account-id", "device-token", DateTime.UtcNow));
             return new DeviceRegistrationRepository(new MssqlContextFactoryFake(), new Mock<IMapper>().Object);
         }
     }
