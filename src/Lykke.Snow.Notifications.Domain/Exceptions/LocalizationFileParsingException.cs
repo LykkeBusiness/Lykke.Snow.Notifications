5a2a71e27edcf944ef5f9312ef323efd166dc771
Fatih Dumanli <fatihdumanli@live.com>
2023-03-24 12:20:40 +0300

    Fix the message

--- src/Lykke.Snow.Notifications.Domain/Exceptions/LocalizationFileParsingException.cs
+++ src/Lykke.Snow.Notifications.Domain/Exceptions/LocalizationFileParsingException.cs
@@ -5,7 +5,7 @@ namespace Lykke.Snow.Notifications.Domain.Exceptions
     public class LocalizationFileParsingException : Exception
     {
         private const string ErrorMsg = @"Could not parse the localization file. 
-            Please make sure it's a valid json file and it respects the LocalizationStore format.";
+            Please make sure it's a valid json file and it respects the LocalizationData format.";
 
         public LocalizationFileParsingException()
         {
