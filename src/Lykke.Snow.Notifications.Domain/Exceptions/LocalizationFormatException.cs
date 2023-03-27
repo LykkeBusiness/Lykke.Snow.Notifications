3e1a4afbc575c05744b7a7bb7f9e7d7d0306d37d
Fatih Dumanli <fatihdumanli@live.com>
2023-03-27 21:30:05 +0300

    Update exception

--- src/Lykke.Snow.Notifications.Domain/Exceptions/LocalizationFormatException.cs
+++ src/Lykke.Snow.Notifications.Domain/Exceptions/LocalizationFormatException.cs
@@ -8,7 +8,7 @@ namespace Lykke.Snow.Notifications.Domain.Exceptions
         public LocalizationFormatException(string notificationType, string lang, string format, IReadOnlyList<string> arguments)
             : base(@$"The number of arguments in the template and given parameters don't match for {notificationType} - {lang}!
                 Format: {format}
-                Arguments: {string.Join(",", arguments)}") 
+                Arguments: {string.Join(", ", arguments)}") 
         {
         }
     }
