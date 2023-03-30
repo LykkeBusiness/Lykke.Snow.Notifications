012da422b8631e2fbdb1f394f091554d6d0313c1
Fatih Dumanli <fatihdumanli@live.com>
2023-03-30 15:02:09 +0300

    Add ActivityHandler / MessagePreviewHandler and add more unit tests

--- src/Lykke.Snow.Notifications.Domain/Services/IActivityHandler.cs
+++ src/Lykke.Snow.Notifications.Domain/Services/IActivityHandler.cs
@@ -3,6 +3,9 @@ using Lykke.MarginTrading.Activities.Contracts.Models;
 
 namespace Lykke.Snow.Notifications.Domain.Services
 {
+    /// <summary>
+    /// Responsible for handling all activity events that coming to the Notification Service
+    /// </summary>
     public interface IActivityHandler
     {
         Task Handle(ActivityEvent e);
