18a4f817c83b7ab3ce18277b72acb7135a3724d4
Fatih Dumanli <fatihdumanli@live.com>
2023-04-04 18:35:48 +0300

    Add unique constraint on (DeviceId, AccountId)

--- src/Lykke.Snow.Notifications.SqlRepositories/EntityConfigurations/DeviceEntityConfiguration.cs
+++ src/Lykke.Snow.Notifications.SqlRepositories/EntityConfigurations/DeviceEntityConfiguration.cs
@@ -12,6 +12,7 @@ namespace Lykke.Snow.Notifications.SqlRepositories.EntityConfigurations
             builder.Property(x => x.Oid).ValueGeneratedOnAdd();
             builder.Property(x => x.DeviceId).HasMaxLength(128).IsRequired();
             builder.HasIndex(x => x.DeviceId);
+            builder.HasIndex(x => new { x.DeviceId, x.AccountId }).IsUnique();
             
             builder.Property(x => x.AccountId).HasMaxLength(128).IsRequired();
             builder.Property(x => x.Locale).HasMaxLength(10).IsRequired();
---
+++ src/Lykke.Snow.Notifications.SqlRepositories/Migrations/20230404153351_UniqueIndexOnDeviceIdAndAccountId.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lykke.Snow.Notifications.SqlRepositories.Migrations
+{
+    public partial class UniqueIndexOnDeviceIdAndAccountId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_DeviceConfigurations_DeviceId_AccountId",
+                schema: "notifications",
+                table: "DeviceConfigurations",
+                columns: new[] { "DeviceId", "AccountId" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_DeviceConfigurations_DeviceId_AccountId",
+                schema: "notifications",
+                table: "DeviceConfigurations");
+        }
+    }
+}
