2b1a18bf4e4d3f8ba46dd59932d0762d84cc711c
atarutin <andrey.tarutin@lykke-business.ch>
2023-04-04 13:38:44 +0300

    feat(LT-4616): add optional proxy to FCM interaction

--- src/Lykke.Snow.Notifications/Settings/NotificationServiceSettings.cs
+++ src/Lykke.Snow.Notifications/Settings/NotificationServiceSettings.cs
@@ -6,15 +6,14 @@ namespace Lykke.Snow.Notifications.Settings
     public class NotificationServiceSettings
     {
         public DbSettings Db { get; set; } = new DbSettings();
-
         [Optional]
         public ClientSettings? NotificationServiceClient { get; set; }
-
         public CqrsSettings Cqrs { get; set; } = new CqrsSettings();
         public FcmSettings Fcm { get; set; } = new FcmSettings();
         public SubscribersSettings? Subscribers { get; set; }
-        
         [Optional]
         public CacheSettings? ConfigurationCache { get; set; }
+        [Optional]
+        public ProxySettings? Proxy { get; set; }
     }
 }
