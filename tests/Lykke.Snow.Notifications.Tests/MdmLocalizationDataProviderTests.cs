26b90fd4936b3b11aca09ade5bcb43206cb5bbee
Fatih Dumanli <fatihdumanli@live.com>
2023-05-23 14:15:48 +0300

    feat(LT-4746): process PR comments

--- tests/Lykke.Snow.Notifications.Tests/MdmLocalizationDataProviderTests.cs
+++ tests/Lykke.Snow.Notifications.Tests/MdmLocalizationDataProviderTests.cs
@@ -3,13 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
-using System.Reflection;
 using System.Threading.Tasks;
 using Lykke.Snow.Mdm.Contracts.Api;
 using Lykke.Snow.Notifications.Domain.Exceptions;
 using Lykke.Snow.Notifications.Domain.Model;
 using Lykke.Snow.Notifications.DomainServices.Services;
 using Lykke.Snow.Notifications.Tests.Fakes;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Internal;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -18,83 +18,59 @@ using Xunit;
 
 namespace Lykke.Snow.Notifications.Tests
 {
-    class LocalizationTestData : IEnumerable<object[]>
-    {
-        public IEnumerator<object[]> GetEnumerator()
-        {
-            const string LocalizationJsonText = @"
-                {
-                    ""Titles"": {
-                        ""AccountLocked"": {
-                           ""en"": ""Account locked"", 
-                           ""es"": ""Coenta bloqueada"", 
-                           ""de"": ""Konto gesperrt"", 
-                        },
-                        ""DepositSucceeded"": {
-                           ""en"": ""Deposit Succeeded"", 
-                           ""es"": ""Depósito exitoso"", 
-                           ""de"": ""Einzahlung erfolgreich"", 
-                        }
-                    },
-                    ""Bodies"": {
-                       ""AccountLocked"": {
-                           ""en"": ""Account has been locked."",
-                           ""es"": ""La cuenta ha sido bloqueada"",
-                           ""de"": ""Konto wurde gesperrt""
-                       }, 
-                       ""DepositSucceeded"": {
-                           ""en"": ""{0}{1} has been deposited to the account {2}."",
-                           ""es"": ""{0}{1} se ha depositado en la cuenta {2}."",
-                           ""de"": ""{0}{1} wurde auf das Konto {2} eingezahlt.""
-                       } 
-                    }
-                }
-            ";
-
-            yield return new object[] { JsonConvert.DeserializeObject<LocalizationData>(LocalizationJsonText)! };
-        }
-
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-    }
-
     public class MdmLocalizationDataProviderTests
     {
+        const string LocalizationJsonText = @"
+            {
+                ""Titles"": {
+                    ""AccountLocked"": {
+                       ""en"": ""Account locked"", 
+                       ""es"": ""Coenta bloqueada"", 
+                       ""de"": ""Konto gesperrt"", 
+                    },
+                    ""DepositSucceeded"": {
+                       ""en"": ""Deposit Succeeded"", 
+                       ""es"": ""Depósito exitoso"", 
+                       ""de"": ""Einzahlung erfolgreich"", 
+                    }
+                },
+                ""Bodies"": {
+                   ""AccountLocked"": {
+                       ""en"": ""Account has been locked."",
+                       ""es"": ""La cuenta ha sido bloqueada"",
+                       ""de"": ""Konto wurde gesperrt""
+                   }, 
+                   ""DepositSucceeded"": {
+                       ""en"": ""{0}{1} has been deposited to the account {2}."",
+                       ""es"": ""{0}{1} se ha depositado en la cuenta {2}."",
+                       ""de"": ""{0}{1} wurde auf das Konto {2} eingezahlt.""
+                   } 
+                }
+            }
+        ";
+
+        class LocalizationTestData : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[] { JsonConvert.DeserializeObject<LocalizationData>(LocalizationJsonText)! };
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
         [Fact]
         public void Initialization_ShouldThrowException_WhenPlatformKeyIsNotProvided()
         {
             Assert.Throws<ArgumentNullException>(() => CreateSut(localizationPlatformKeyArg: ""));
             Assert.Throws<ArgumentNullException>(() => CreateSut(localizationPlatformKeyArg: null));
         }
-
-        [Fact]
-        public void CacheExpirationTime_ShouldSetToDefault_WhenNotProvided()
-        {
-            var sut = CreateSut();
-            
-            var cacheExpirationPeriod = sut
-                .GetType()
-                .GetField("_cacheExpirationPeriod", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
-                ?.GetValue(sut);    
-            
-            var defaultCacheExpirationPeriod = sut
-                .GetType()
-                .GetField("DefaultCacheExpirationPeriod", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
-                ?.GetValue(sut);
-            
-            if(cacheExpirationPeriod == null)
-                throw new NullReferenceException($"{nameof(cacheExpirationPeriod)} is null");
-
-            if(defaultCacheExpirationPeriod == null)
-                throw new NullReferenceException($"{nameof(defaultCacheExpirationPeriod)} is null");
-            
-            Assert.Equal((TimeSpan) cacheExpirationPeriod, (TimeSpan) defaultCacheExpirationPeriod);
-        }
         
         #region Load()
         [Fact]
         public async Task Load_ShouldLoadLocalizationFileFromApi_IfLocalizationDataIsNotInitialized()
         {
-            var stub = new LocalizationFilesBinaryApiStub();
+            var stub = new LocalizationFilesBinaryApiStub(LocalizationJsonText);
             
             // create the class - the localization data will be null initially
             var sut = CreateSut(stub);
@@ -104,93 +80,6 @@ namespace Lykke.Snow.Notifications.Tests
             // assert that the api has been called
             Assert.Equal(1, stub.NumOfCalls);
         }
-        
-        [Theory]
-        [ClassData(typeof(LocalizationTestData))]
-        public async Task Load_ShouldReturnLocalizationDataFromMemory_IfDataHasNotExpired(LocalizationData localizationData)
-        {
-            // set expiration to 1 minute
-            var expiration = new TimeSpan(0, 1, 0);
-            
-            var mockApi = new Mock<ILocalizationFilesBinaryApi>();
-            
-            var sut = CreateSut(cacheExpirationPeriodArg: expiration, localizationFilesBinaryApiArg: mockApi.Object);
-
-            // Set value for 'localizationData' private field so that it is not null
-            var dataField = typeof(MdmLocalizationDataProvider).GetField("_localizationData", BindingFlags.NonPublic | BindingFlags.Instance);
-            if(dataField == null)
-                throw new NullReferenceException($"{nameof(dataField)} is null");
-            dataField.SetValue(sut, localizationData);
-            
-            //Set 'lastUpdated' private field to simulate a fresh (30 seconds old) localizationData
-            var lastUpdatedField = typeof(MdmLocalizationDataProvider).GetField("_lastUpdatedAt", BindingFlags.NonPublic | BindingFlags.Instance);
-            if(lastUpdatedField == null)
-                throw new NullReferenceException($"{nameof(lastUpdatedField)} is null");
-            lastUpdatedField.SetValue(sut, DateTime.UtcNow.AddSeconds(-30));
-            
-            var result = await sut.Load();
-            
-            // Verify that api has not been called
-            mockApi.Verify(x => x.GetActiveLocalizationFileAsync(It.IsAny<string>()), Times.Never);
-            
-            Assert.Equal(localizationData, result);
-        }
-        
-        [Theory]
-        [ClassData(typeof(LocalizationTestData))]
-        public async Task Load_ShouldLoadLocalizationFileFromApi_IfDataHasExpired(LocalizationData localizationData)
-        {
-            // set expiration to 1 minute
-            var expiration = new TimeSpan(0, 1, 0);
-
-            var stub = new LocalizationFilesBinaryApiStub();
-
-            var sut = CreateSut(cacheExpirationPeriodArg: expiration, localizationFilesBinaryApiArg: stub);
-
-            // Set value for 'localizationData' private field so that it is not null
-            var dataField = typeof(MdmLocalizationDataProvider).GetField("_localizationData", BindingFlags.NonPublic | BindingFlags.Instance);
-            if(dataField == null)
-                throw new NullReferenceException($"{nameof(dataField)} is null");
-            dataField.SetValue(sut, localizationData);
-
-            //Set 'lastUpdated' private field to simulate a stale (90 seconds old) localizationData
-            var lastUpdatedField = typeof(MdmLocalizationDataProvider).GetField("_lastUpdatedAt", BindingFlags.NonPublic | BindingFlags.Instance);
-            if(lastUpdatedField == null)
-                throw new NullReferenceException($"{nameof(lastUpdatedField)} is null");
-            lastUpdatedField.SetValue(sut, DateTime.UtcNow.AddSeconds(-90));
-            
-            await sut.Load();
-            
-            Assert.Equal(1, stub.NumOfCalls);
-        }
-        
-
-        [Fact]
-        public async Task Load_ShouldUpdateLastUpdatedAt_AfterUpdatingTheCache()
-        {
-            var stub = new LocalizationFilesBinaryApiStub();
-            
-            var simulatedNow = new DateTime(2023, 05, 18, 14, 51, 22);
-
-            var mockSystemClock = new Mock<ISystemClock>();
-            mockSystemClock.Setup(x => x.UtcNow).Returns((DateTimeOffset)simulatedNow);
-
-            // Create the sut without (the localizationData will be null initially - so that it will load the data from API)
-            var sut = CreateSut(localizationFilesBinaryApiArg: stub, systemClockArg: mockSystemClock.Object);
-            
-            await sut.Load();
-
-            // read lastUpdatedAt value
-            var lastUpdatedAt = sut
-                .GetType()
-                .GetField("_lastUpdatedAt", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
-                ?.GetValue(sut);
-            
-            if (lastUpdatedAt == null)
-                throw new NullReferenceException($"{nameof(lastUpdatedAt)} is null");
-            
-            Assert.Equal(simulatedNow, (DateTime) lastUpdatedAt);
-        }
         #endregion
         
 
@@ -198,7 +87,7 @@ namespace Lykke.Snow.Notifications.Tests
         [Fact]
         public async Task LoadFromMdm_HappyPath_ShouldReturnParsedLocalizationData()
         {
-            var stub = new LocalizationFilesBinaryApiStub();
+            var stub = new LocalizationFilesBinaryApiStub(LocalizationJsonText);
             
             var sut = CreateSut(localizationFilesBinaryApiArg: stub);
             
@@ -225,7 +114,7 @@ namespace Lykke.Snow.Notifications.Tests
         public async Task LoadFromMdm_ShouldThrowLocalizationFileParsingException_IfResultIsNull()
         {
             // setup the stub so it simulates the api returning an empty string
-            var stub = new LocalizationFilesBinaryApiStub();
+            var stub = new LocalizationFilesBinaryApiStub(LocalizationJsonText);
             stub.SetJsonResponseText("");
             
             var sut = CreateSut(localizationFilesBinaryApiArg: stub);
@@ -237,7 +126,7 @@ namespace Lykke.Snow.Notifications.Tests
         public async Task LoadFromMdm_ShouldThrowLocalizationFileParsingException_IfJsonParsingFails()
         {
             // setup the stub so it returns an invalid json (not parsable into LocalizationData)
-            var stub = new LocalizationFilesBinaryApiStub();
+            var stub = new LocalizationFilesBinaryApiStub(LocalizationJsonText);
             stub.SetJsonResponseText(@"{""FirstName"": ""Jane"", ""LastName"": ""Doe""}");
             
             var sut = CreateSut(localizationFilesBinaryApiArg: stub);
@@ -256,7 +145,7 @@ namespace Lykke.Snow.Notifications.Tests
             
             ILocalizationFilesBinaryApi localizationFilesBinaryApi = new Mock<ILocalizationFilesBinaryApi>().Object;
             TimeSpan cacheExpirationPeriod = TimeSpan.FromMinutes(5);
-            ISystemClock systemClock = new SystemClock();
+            var memoryCache = new MemoryCache(new MemoryCacheOptions());
             
             if(localizationFilesBinaryApiArg != null)
             {
@@ -268,13 +157,11 @@ namespace Lykke.Snow.Notifications.Tests
                 cacheExpirationPeriod = cacheExpirationPeriodArg.Value;
             }
             
-            if(systemClockArg != null)
-            {
-                systemClock = systemClockArg;
-            }
-            
-            return new MdmLocalizationDataProvider(mockLogger.Object, localizationFilesBinaryApi, cacheExpirationPeriodArg, systemClock,
-                localizationPlatformKey: localizationPlatformKeyArg);
+            return new MdmLocalizationDataProvider(mockLogger.Object, 
+                localizationFilesBinaryApi, 
+                cacheExpirationPeriodArg, 
+                localizationPlatformKey: localizationPlatformKeyArg,
+                cache: memoryCache);
         }
     }
 }
