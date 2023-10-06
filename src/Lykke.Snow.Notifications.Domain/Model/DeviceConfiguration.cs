5dd94a3171865353cc68739a13c797f4329dc59a
Aliaksandr Vashetsin <aliaksandr.vashetsin@lykke-business.ch>
2023-10-06 16:32:10 +0300

    LT-5002: fix last updated

--- src/Lykke.Snow.Notifications.Domain/Model/DeviceConfiguration.cs
+++ src/Lykke.Snow.Notifications.Domain/Model/DeviceConfiguration.cs
@@ -148,6 +148,7 @@ namespace Lykke.Snow.Notifications.Domain.Model
             AccountId = accountId;
             Locale = localeParsed;
             Notifications = notifications ?? new List<Notification>();
+            LastUpdated = DateTime.UtcNow;
         }
 
         public override string ToString()
